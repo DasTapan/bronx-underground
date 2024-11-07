adad7c3aac53c4856e1d6a9ce657412b69390730
DasTapan <tapands.das@gmail.com>
2024-11-07 15:19:09 +0530

    Conditionally render attend event link

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
-    
+    before_action :authenticate_user!, only: [ :new, :create ]
+
     def index
         @events = Event.all
     end
@@ -8,6 +8,12 @@ class EventsController < ApplicationController
     def show
         @created_event = Event.find(params[:id])
         @attendees = @created_event.attendees
+        attendee_ids = @created_event.attendees.pluck(:id)
+        @is_an_attendee = if get_current_user_id.nil?
+            nil
+        else
+            attendee_ids.include?(get_current_user_id)
+        end
     end
 
     def new
@@ -16,7 +22,7 @@ class EventsController < ApplicationController
 
     def create
         @created_event = current_user.created_events.build(event_params)
-        
+
         if @created_event.save
             redirect_to @created_event
         else
@@ -27,6 +33,14 @@ class EventsController < ApplicationController
     private
 
     def event_params
-        params.require(:event).permit(:date,:title,:time)
+        params.require(:event).permit(:date, :title, :time)
+    end
+
+    def get_current_user_id
+        if signed_in?
+            current_user.id
+        else
+            nil
+        end
     end
 end
