f6e906351e814ae1c16925bb2208eb2f529291b9
DasTapan <tapands.das@gmail.com>
2024-11-12 13:07:31 +0530

    Update to use scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     has_many :bookings
     belongs_to :creator, class_name: 'User'
     has_many :attendees, through: :bookings
+
+    scope :past, -> {where("event_at < ?", Time.current)}
+    scope :future, -> {where("event_at >= ?", Time.current)}
 end
